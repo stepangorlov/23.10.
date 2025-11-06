12611c9f0e4e7295b6af4291e696d64586b1fa2b
stepangorlov <stepangorlov81@gmail.com>
2025-11-07 00:36:20 +0300

    Create LINEAR SEARCH.java

---
+++ docs/LINEAR SEARCH.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        int[] array = {3, 5, 2, 7, 9, 1, 4};
+        int target = 7;
+        
+        int result = linearSearch(array, target);
+        
+        if (result != -1) {
+            System.out.println("Элемент найден на позиции: " + result);
+        } else {
+            System.out.println("Элемент не найден");
+        }
+    }
+}
