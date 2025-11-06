4812798644af1137a2a7baa8574c5f626da543cf
stepangorlov <stepangorlov81@gmail.com>
2025-11-06 23:13:07 +0300

    Create INSERTION SORT.cpp

---
+++ docs/INSERTION SORT.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(const vector<int>& arr) {
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {12, 11, 13, 5, 6};
+    cout << "Исходный массив: ";
+    printArray(arr);
+    
+    insertionSort(arr);
+    
+    cout << "Отсортированный массив: ";
+    printArray(arr);
+    
+    return 0;
+}
