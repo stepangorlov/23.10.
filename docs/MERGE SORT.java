52cd7cb09308a7b38e262637a34668e2b9125b34
stepangorlov <stepangorlov81@gmail.com>
2025-11-06 23:13:40 +0300

    Create MERGE SORT.java

---
+++ docs/MERGE SORT.java
@@ -0,0 +1,57 @@
+public class MergeSort {
+    public static void mergeSort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+        
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+        
+        System.arraycopy(arr, 0, left, 0, left.length);
+        System.arraycopy(arr, mid, right, 0, right.length);
+        
+        mergeSort(left);
+        mergeSort(right);
+        
+        merge(arr, left, right);
+    }
+    
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        System.out.print("Исходный массив: ");
+        printArray(arr);
+        
+        mergeSort(arr);
+        
+        System.out.print("Отсортированный массив: ");
+        printArray(arr);
+    }
+    
+    private static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
